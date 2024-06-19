1b83ad8d0a86eb27f2a4f3a43d2e3516da417ee8
stephenthoma <stephen@thoma.io>
2024-06-19 10:25:43 -0700

    Implement telescope multi select

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
+local select_one_or_multi = function(prompt_bufnr)
+	local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+	local multi = picker:get_multi_selection()
+	if not vim.tbl_isempty(multi) then
+		require("telescope.actions").close(prompt_bufnr)
+		for _, j in pairs(multi) do
+			if j.path ~= nil then
+				vim.cmd(string.format("%s %s", "edit", j.path))
+			end
+		end
+	else
+		require("telescope.actions").select_default(prompt_bufnr)
+	end
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
 	module = "telescope",
@@ -54,6 +69,7 @@ return {
 					i = {
 						["<esc>"] = actions.close,
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+						["<CR>"] = select_one_or_multi,
 					},
 					n = {
 						["q"] = actions.close,
@@ -68,6 +84,10 @@ return {
 					mappings = {
 						["n"] = {
 							["-"] = fb_actions.goto_parent_dir,
+							["<CR>"] = select_one_or_multi,
+						},
+						["i"] = {
+							["<CR>"] = select_one_or_multi,
 						},
 					},
 				},
