da8cd1d104ada033250aed9920c3aa7dbca5190e
stephenthoma <stephen@thoma.io>
2024-07-08 09:42:19 -0700

    Improve appearance of indent line

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,28 +1,32 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-        require("ibl").setup({
-            scope = { enabled = true },
-            whitespace = { remove_blankline_trail = false },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "startify",
-                    "aerial",
-                    "alpha",
-                    "dashboard",
-                    "packer",
-                    "neogitstatus",
-                    "NvimTree",
-                    "neo-tree",
-                    "Trouble",
-                },
-                buftypes = {
-                    "nofile",
-                    "terminal",
-                },
-            }
-        })
-    end,
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			scope = { enabled = true, highlight = { "DiagnosticInfo" } },
+			whitespace = { remove_blankline_trail = false },
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			exclude = {
+				filetypes = {
+					"help",
+					"startify",
+					"aerial",
+					"alpha",
+					"dashboard",
+					"packer",
+					"neogitstatus",
+					"NvimTree",
+					"neo-tree",
+					"Trouble",
+				},
+				buftypes = {
+					"nofile",
+					"terminal",
+				},
+			},
+		})
+	end,
 }
