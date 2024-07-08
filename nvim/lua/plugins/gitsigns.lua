4189497076c586e7a455e9147219441785f4a226
stephenthoma <stephen@thoma.io>
2024-07-08 09:41:58 -0700

    Add key to reset hunk (gr)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,10 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require('gitsigns').setup()
-    end,
-    keys = {
-        { "<leader>cb", "<cmd>Gitsigns blame_line<CR>", desc = "Show blame line" },
-    },
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+	end,
+	keys = {
+		{ "<leader>cb", "<cmd>Gitsigns blame_line<CR>", desc = "Show blame line" },
+		{ "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset git hunk" },
+	},
 }
