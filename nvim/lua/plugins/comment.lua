ee6246425f4924424c7251ee2643684360206ebd
stephenthoma <stephen@thoma.io>
2024-07-08 09:41:19 -0700

    Update tsx comment setup

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,16 +1,16 @@
 return {
-    'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    },
-    keys = {
-        {"<leader>/", "<cmd> lua require('Comment.api').toggle.linewise.current()<CR>", desc = "Toggle line comment" },
-    },
-    lazy = false,
-    config = function()
-	    require('Comment').setup {
-		    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-	    }
-    end
+	"numToStr/Comment.nvim",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring", -- make commenting code work correctly with embedded languages
+		"windwp/nvim-ts-autotag", -- autoclose HTML tags
+	},
+	keys = {
+		{ "<leader>/", "<cmd> lua require('Comment.api').toggle.linewise.current()<CR>", desc = "Toggle line comment" },
+	},
+	lazy = false,
+	config = function()
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
-
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,38 @@
+vim.g.skip_ts_context_commentstring_module = true
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring", -- make commenting code work correctly with embedded languages
-        "windwp/nvim-ts-autotag" -- autoclose HTML tags
-    },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = "all",
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            },
-            autotag = { enable = true },
-            incremental_selection = { enable = true },
-            indent = { enable = false },
-        })
-    end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring", -- make commenting code work correctly with embedded languages
+			"windwp/nvim-ts-autotag", -- autoclose HTML tags
+		},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+				autotag = { enable = true },
+				incremental_selection = { enable = true },
+				indent = { enable = false },
+			})
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring", -- make commenting code work correctly with embedded languages
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+				languages = {
+					typescript = "// %s",
+				},
+			})
+		end,
+	},
 }
